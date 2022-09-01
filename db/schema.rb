0d64c781c322bdbdee8db9d839263a1ce0de1976
Brandon Newman <99927941+Brandon-Newman@users.noreply.github.com>
2022-09-01 13:56:53 -0500

    Ruby Rails Lesson 17: Migrations Revisited

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_221915) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_183600) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
@@ -19,6 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_221915) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.date "released_on"
+    t.string "director"
+    t.string "duration"
+    t.string "image_file_name", default: "placeholder.png"
   end
 
 end
