06f1de50224334238754b2925eaaad39761bc104
Brandon Newman <99927941+Brandon-Newman@users.noreply.github.com>
2022-09-06 15:09:04 -0500

    Ruby Rails Lesson 19: Handling Validation Errors

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,45 +1,50 @@
 class MoviesController < ApplicationController
-    def index
-      @movies = Movie.released
-    end
-  
-    def show
-        @movie = Movie.find(params[:id])
-    end
+  def index
+    @movies = Movie.released
+  end
 
-    def edit
+  def show
       @movie = Movie.find(params[:id])
-    end
+  end
 
-    def new
-      @movie = Movie.new
-    end
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
 
 def update
   @movie = Movie.find(params[:id])
-  @movie.update(movie_params)
-  redirect_to @movie
+  if @movie.update(movie_params)
+    redirect_to @movie
+  else
+    render :edit, status: :unprocessable_entity
+  end
 end
 
 def create
   @movie = Movie.new(movie_params)
-  @movie.save
-  redirect_to @movie
+  if @movie.save
+    redirect_to @movie
+  else
+    render :new, status: :unprocessable_entity
+  end
 end
 
 def destroy
-  @movie = Movie.find(params[:id])
-  @movie.destroy
-  redirect_to movies_url, status: :see_other
+@movie = Movie.find(params[:id])
+@movie.destroy
+redirect_to movies_url, status: :see_other
 end
 
 private
 
-  def movie_params
-    params.require(:movie).
-      permit(:title, :description, :rating, :released_on, :total_gross,
-      :director, :duration, :image_file_name)
-  end
-    
-  end
+def movie_params
+  params.require(:movie).
+    permit(:title, :description, :rating, :released_on, :total_gross,
+    :director, :duration, :image_file_name)
+end
   
+end
