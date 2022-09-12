e722b8ab5d69a8eb3a6c5cc7ffcbf307349d0cb3
Brandon Newman <99927941+Brandon-Newman@users.noreply.github.com>
2022-09-12 14:47:09 -0500

    Ruby Rails Lesson 27: User Signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   validates :email, presence: true,
-            format: { with: /\S+@\S+/ },
-            uniqueness: { case_sensitive: false }
+                    format: { with: /\S+@\S+/ },
+                    uniqueness: { case_sensitive: false }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
 
   resources :movies
 
+  get "signup" => "users#new"
+
+
 end
