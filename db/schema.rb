fdb338d10fd42548754a5ce1f006814b02e93c28
Brandon Newman <99927941+Brandon-Newman@users.noreply.github.com>
2022-09-14 14:37:32 -0500

    Ruby Rails Lesson 34: Admin Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_11_165942) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_14_192658) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_11_165942) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "reviews", "movies"
