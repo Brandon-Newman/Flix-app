e2ce3c93f3319711369dbc4e449c32f4e3f21980
Brandon Newman <99927941+Brandon-Newman@users.noreply.github.com>
2022-09-21 14:50:57 -0500

    Ruby Rails: Done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   before_action :require_signin, except: [:index, :show]
   before_action :require_admin, except: [:index, :show]
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     case params[:filter]
     when "upcoming"
@@ -15,54 +15,54 @@ class MoviesController < ApplicationController
   end
 
   def show
-      @fans = @movie.fans
-      @genres = @movie.genres.order(:name)
+    @fans = @movie.fans
+    @genres = @movie.genres.order(:name)
 
-      if current_user
-        @favorite = current_user.favorites.find_by(movie_id: @movie.id)
-      end
+    if current_user
+      @favorite = current_user.favorites.find_by(movie_id: @movie.id)
+    end
   end
 
   def edit
   end
 
+  def update
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie successfully updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def new
+    @movie = Movie.new
   end
 
-def update
-  if @movie.update(movie_params)
-    redirect_to @movie, notice: "Movie successfully updated!"
-  else
-    render :edit, status: :unprocessable_entity
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: "Movie successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-end
 
-def create
-  @movie = Movie.new(movie_params)
-  if @movie.save
-    redirect_to @movie, notice: "Movie successfully created!"
-  else
-    render :new, status: :unprocessable_entity
+  def destroy
+    @movie.destroy
+    redirect_to movies_url, status: :see_other,
+      alert: "Movie successfully deleted!"
   end
-end
-
-def destroy
-  @movie = Movie.find(params[:id])
-  @movie.destroy
-  redirect_to movies_url, status: :see_other,
-    alert: "Movie successfully deleted!"
-end
 
 private
 
-def movie_params
-  params.require(:movie).
-    permit(:title, :description, :rating, :released_on, :total_gross,
-    :director, :duration, :image_file_name, genre_ids: [])
-end
+  def set_movie
+    @movie = Movie.find_by!(slug: params[:id])
+  end
 
-def set_movie
-  @movie = Movie.find_by!(slug: params[:id])
-end
-  
+  def movie_params
+    params.require(:movie).
+      permit(:title, :description, :rating, :released_on, :total_gross,
+             :director, :duration, :image_file_name, genre_ids: [])
+  end
+
 end
